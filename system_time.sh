828ca5e1c2dcb0daf0dc3ca5de1c658e2712a922
harshith1817 <127752210+harshith1817@users.noreply.github.com>
2023-06-06 12:02:04 +0530

    Week2B

---
+++ system_time.sh
@@ -0,0 +1,13 @@
+hour=`date +%H`
+if [ $hour -lt 12 ]
+then
+echo "GOOD MORNING WORLD"
+elif [ $hour -le 16 ]
+then
+echo "GOOD AFTERNOON WORLD"
+elif [ $hour -le 20 ]
+then
+echo "GOOD EVENING WORLD"
+else
+echo "GOOD NIGHT WORLD"
+fi
